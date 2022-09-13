1746b2c7a4c9f14651862e58d3d8854140f659fa
wongpakho814 <wongpakho814@gmail.com>
2022-09-13 16:50:58 +1000

    Added correct color class to the description field using js

--- Develop/script.js
+++ Develop/script.js
@@ -3,17 +3,37 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 // Creates every elements inside the time block
 function createTimeBlock(time, meridian) {
-    let timeBlockEl = $("<div>", {id: "hour-" + time, "class": "row time-block"});
+    let timeBlockEl = $("<div>", {id: "block-" + time + meridian, "class": "row time-block"});
     let hourEl = $("<div>", {"class": "hour col-1 m-auto"});
     // Check if the current time is 12am or 12pm
     (time === 0) ? hourEl.text("12" + meridian) : hourEl.text(time + meridian);
     let descriptionEl = $("<textarea>", {"class": "description col-10"});
+    descriptionEl.addClass(descriptionColor(time, meridian)); 
     let saveBtnEl = $("<div>", {"class": "saveBtn col-1 m-auto fa fa-save"});
 
     timeBlockEl.append(hourEl);
     timeBlockEl.append(descriptionEl);
     timeBlockEl.append(saveBtnEl);
     $(".container-fluid").append(timeBlockEl);
+}
+
+// Check the current time and add the correct color to the description field
+function descriptionColor(time, meridian) {
+    let currTime = moment().hours();
+    // Change am/pm to 24-hours clock
+    if (meridian === "PM") {
+        time += 12;
+    }
+    
+    if (time < currTime) {
+        return "past";
+    }
+    else if (time === currTime) {
+        return "present";
+    }
+    else {
+        return "future";
+    }
 }
 
 // Initialize the webpage by rendering all time blocks
